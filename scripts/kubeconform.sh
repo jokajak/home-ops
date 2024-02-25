4c0c095284c66a47af4a25e9c677192b6578f48a
Josh <jokajak@gmail.com>
2024-02-25 15:03:46 -0500

    fix(kubeconform): strip sops from validation

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -11,10 +11,6 @@ kustomize_config="kustomization.yaml"
 kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
-    "-ignore-filename-pattern"
-    "*.sops."
-    "-skip"
-    "Secret"
     "-schema-location"
     "default"
     "-schema-location"
@@ -36,6 +32,7 @@ find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IF
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
+      yq "del(.sops)" | \
       kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
@@ -47,6 +44,7 @@ find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IF
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
+      yq "del(.sops)" | \
       kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
