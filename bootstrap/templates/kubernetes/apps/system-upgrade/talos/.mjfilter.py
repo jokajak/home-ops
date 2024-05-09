9ed000f136b7ff4e628d1b33db89218ff6b4fd5e
Josh <jokajak@gmail.com>
2024-05-09 06:57:35 -0400

    chore: remove bootstrap dir
    this will reduce the renovate churn

--- bootstrap/templates/kubernetes/apps/system-upgrade/talos/.mjfilter.py
+++
@@ -1,6 +0,0 @@
-main = lambda data: (
-    data.get("distribution", {}).get("type", "k3s") in ["talos"] and
-        data.get("distribution", {})
-            .get("talos", {})
-            .get("schematicID", {})
-)
